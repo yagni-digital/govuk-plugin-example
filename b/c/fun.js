acccbde831c582eb9c635982fb2cb290f122af8f
Mat Carey <mat+yagni-digital@matcarey.co.uk>
2018-08-02 09:52:03 +0100

    Adding an example showing the full functionality of the hooks.

--- b/c/fun.js
+++ b/c/fun.js
@@ -1,2 +1,13 @@
-window.SHARED_VARIABLE = 'abcdefghij'
 
+window.MY_NAMESPACE = window.MY_NAMESPACE || {}
+
+window.MY_NAMESPACE.demoFeature = function (selector) {
+  window.$(selector).on('click', function (e) {
+    e.preventDefault();
+    window.alert('The behaviour from z-govuk-plugin-example is used - a liongit sta');
+  })
+}
+
+window.$(document).on('ready', function () {
+  window.MY_NAMESPACE.demoFeature('.demo-feature')
+})

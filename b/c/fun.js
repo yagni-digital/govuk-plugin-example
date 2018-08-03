140b0eda892be314b37c2d0253cf83694dc58dcc
Mat Carey <mat@yagni.digital>
2018-08-03 13:02:55 +0100

    Update fun.js

--- b/c/fun.js
+++ b/c/fun.js
@@ -4,7 +4,7 @@ window.MY_NAMESPACE = window.MY_NAMESPACE || {}
 window.MY_NAMESPACE.demoFeature = function (selector) {
   window.$(selector).on('click', function (e) {
     e.preventDefault();
-    window.alert('The behaviour from z-govuk-plugin-example is used - a liongit sta');
+    window.alert('The behaviour from z-govuk-plugin-example is used - a lion');
   })
 }
 
